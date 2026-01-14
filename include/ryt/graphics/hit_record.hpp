6e5aa646cf261b9067256d7d4476329796bf0834
k-54th15h <sathish061006@gmail.com>
2026-01-14 23:39:43 +0530

    add front_face detection

--- include/ryt/graphics/hit_record.hpp
+++ include/ryt/graphics/hit_record.hpp
@@ -2,6 +2,7 @@
 #define HIT_RECORD_H
 
 #include <ryt/math/vec3.hpp>
+#include <ryt/math/ray.hpp>
 
 namespace ryt
 {
@@ -12,6 +13,14 @@ namespace ryt
 	    vec3 p;
 	    vec3 normal;
 	    double t;
+	    bool front_face;
+
+	    void set_face_normal(const ray& r, const vec3& outward_normal)
+	    {
+		front_face = dot(r.direction(), outward_normal) < 0;
+
+		normal = (front_face) ? outward_normal : -outward_normal;
+	    }
     };
 }
 
--- include/ryt/graphics/sphere.hpp
+++ include/ryt/graphics/sphere.hpp
@@ -43,7 +43,9 @@ namespace ryt
 		// record hit then return true
 		rec.t = root;
 		rec.p = r.at(rec.t);
-		rec.normal = (rec.p - center) / radius;
+
+		vec3 outward_normal = (rec.p - center) / radius;
+		rec.set_face_normal(r, outward_normal);
 
 		return true;
 	    }
