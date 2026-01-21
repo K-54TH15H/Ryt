1959a70472c504d1f21b38eb9288a64ae8ea9152
k-54th15h <sathish061006@gmail.com>
2026-01-22 00:26:59 +0530

    add color.cpp

---
+++ src/graphics/color.cpp
@@ -0,0 +1,30 @@
+#include <ryt/rtcore.hpp>
+#include <cmath>
+
+namespace RYT
+{
+    void LinearToGamma(double& linearComponent)
+    {
+	linearComponent = (linearComponent > 0) ? std::sqrt(linearComponent) : 0;
+    }
+
+    void WriteColor(std::ostream& os, const Color& pixelColor)
+    {
+	auto r = pixelColor.x;
+	auto g = pixelColor.y;
+	auto b = pixelColor.z;
+
+	// Gamma correction
+	LinearToGamma(r);
+	LinearToGamma(g);
+	LinearToGamma(b);
+
+	static const Interval intensity(0.000, 0.999);
+
+	int rb = int(256 * intensity.Clamp(r));
+	int gb = int(256 * intensity.Clamp(g));
+	int bb = int(256 * intensity.Clamp(b));
+
+	os << rb << ' ' << gb << ' ' << bb << std::endl;
+    }
+}
